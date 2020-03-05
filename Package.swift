e8369493cc37f47f7fc075c3eb156a1b5adbc13d
HanXianzhao <saynotta@gmail.com>
2020-03-05 13:30:55 +0800

    Support xcode spm

--- Package.swift
+++ Package.swift
@@ -3,5 +3,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Pantomime"
+    name: "Pantomime",
+    products: [
+        .library(name: "Pantomime", targets: ["Pantomime"]),
+    ],
+    targets: [
+        .target(name: "Pantomime", path: "sources"),
+        .testTarget(name: "PantomimeTests", dependencies: ["Pantomime"])
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
 )
